421074b874584eccb3730cde9392201350ba62ef
David Florez Mazuera <pandafm200@gmail.com>
2024-02-23 20:20:59 +0100

    Edit profile completed

--- front-end/src/pages/api/auth/edit.ts
+++ front-end/src/pages/api/auth/edit.ts
@@ -1,22 +1,82 @@
 export const prerender = false
 import type { APIRoute } from "astro";
 import { supabase } from "../../../lib/supabase";
+import { JSDOM } from 'jsdom';
+import DOMPurify from 'dompurify';
+import { jwtDecode } from "jwt-decode";
+import type { JWToken } from "../../../types/supabase";
 
-export const POST: APIRoute = async ({ request }) => {
+export const POST: APIRoute = async ({ request, cookies }) => {
   const formData = await request.formData();
-  const username = formData.get("username")?.toString();
-  const supportLink = formData.get("support_link")?.toString();
-
-  //const actualUsername = await supabase.from("base_user").select("username");
-
-  const { error } = await supabase.from("users").update({
-    username: username,
-    support_link: supportLink,
-  });
-
-  if (error) {
-    return new Response(error.message, { status: 500 });
+  const purify = DOMPurify(new JSDOM().window);
+  
+  
+  const token:string | undefined= cookies.get("sb-access-token")?.value;
+  if (token == undefined) {
+    return new Response("Unauthorized", { status: 401 });
   }
+  const decoded = jwtDecode<JWToken>(token);
+  const id = decoded.sub;
 
-  return new Response("Success", { status: 200 });
+  let toReturn = {};
+  const isCreador = cookies.get("iscreador");
+  if(isCreador === undefined){
+    const username = formData.get("username")?.toString();
+    if(username === undefined){
+      return new Response("Username is required", { status: 400 });
+    }
+    const cleanedUsername = purify.sanitize(username);
+
+    const baseUserResponse = await supabase.from("base_user").update({
+      username: cleanedUsername,
+    })
+    .eq("id", id);
+
+    if (baseUserResponse.error) {
+      return new Response("Error while updating username", { status: 500 });
+    }
+    toReturn = {
+      username: cleanedUsername,
+    }
+  }
+  else{
+    const username = formData.get("username")?.toString();
+    if(username === undefined){
+      return new Response("Username is required", { status: 400 });
+    }
+    const cleanedUsername = purify.sanitize(username);
+    const supportLink = formData.get("support_link")?.toString();
+    if(supportLink === undefined){
+      return new Response("Support link is required", { status: 400 });
+    }
+    const clenaedSupportLink = purify.sanitize(supportLink);
+    
+    const baseUserResponse = await supabase.from("base_user").update({
+      username: cleanedUsername,
+    })
+    .eq("id", id);
+
+    if (baseUserResponse.error) {
+      return new Response("Error while updating username", { status: 500 });
+    }
+
+    const creadorResponse = await supabase.from("creador").update({
+      support_link: clenaedSupportLink,
+    })
+    .eq("id", id);
+
+    if (creadorResponse.error) {
+      return new Response("Error while updating suportLink", { status: 500 });
+    }
+    // const answer = {
+    //   username: baseUserResponse.data?.username,
+    //   support_link: supportLink,
+    // }
+    toReturn = {
+      username: cleanedUsername,
+      support_link: clenaedSupportLink,
+    }
+  }
+  
+  return new Response( JSON.stringify(toReturn), { status: 200 });
 };
